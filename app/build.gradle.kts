7d7904ca0c2b75035506a44105f3066599673f2e
Iaroslav Belozerov <yaabelozerov@edu.hse.ru>
2025-02-12 12:46:41 +0300

    add gradle plugins

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.kotlinx.serialization) apply false
 }
 
 android {
